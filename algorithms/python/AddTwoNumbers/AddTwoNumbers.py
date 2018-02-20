2abeb3bd9a13eb5f735e5b775fde76095026f47c
Bohan Shan <yupsno@gmail.com>
2018-02-20 18:40:40 -0500

    Update AddTwoNumbers.py

--- algorithms/python/AddTwoNumbers/AddTwoNumbers.py
+++ algorithms/python/AddTwoNumbers/AddTwoNumbers.py
@@ -1,3 +1,22 @@
+# Source : https://leetcode.com/problems/add-two-numbers/
+# Author : Bohan Shan
+# Date   : 2018-02-20
+    
+'''
+You are given two non-empty linked lists representing two non-negative integers. 
+The digits are stored in reverse order and each of their nodes contain a single digit.
+Add the two numbers and return it as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+Example
+
+Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+Output: 7 -> 0 -> 8
+Explanation: 342 + 465 = 807.
+'''
+    
+
 # Definition for singly-linked list.
 # class ListNode(object):
 #     def __init__(self, x):
@@ -5,8 +24,6 @@
 #         self.next = None
 
 class Solution(object):
-
-
 
     def addTwoNumbers (self, l1, l2):
         """
@@ -18,8 +35,8 @@ class Solution(object):
         sum1 = num1.val;
         num2 = l2;
         sum2 = num2.val;
-
         count = 1;
+        
         while(num1.next != None):
             sum1 += num1.next.val * 10**count;
             num1 = num1.next;
